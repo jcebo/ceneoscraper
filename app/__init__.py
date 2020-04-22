3bbb629d2b172f5f2b6100939a77aba36b91ccbf
jcebo <kubac0208@gmail.com>
2020-04-22 13:13:02 +0200

    2020-04-22 J.C.

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+#import bibliotek
+from flask import Flask
+#utwotrzenie instancji (obiektu) klasy Flask reprezentującej aplikację
+app = Flask(__name__)
+
+#from app import routes
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8081)
