9ca23968d633404a921ba7cccf75a2b9b5af6a16
jcebo <kubac0208@gmail.com>
2020-05-20 13:05:41 +0200

    J.C. 2020-05-20

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+
+
+import utils
 class Product:
     def __init__(self, product_id=None, name=None, opinions=[]):
         self.product_id = product.id
@@ -16,11 +19,12 @@ class Product:
             url_prefix = "https://www.ceneo.pl"
             url_postfix = "#tab=reviews"
             url = url_prefix+"/"+self.product_id+url_postfix
+            print(url)
             while url:
                 #pobranie kodu HTML strony z adresu URL 
                 page_response =  requests.get(url)
                 page_tree = BeautifulSoup(page_response.text, 'html.parser')
-                opinions = page_tree.select("li.js_product-review")
+                opinions = page_tree.select("div.js_product-review")
                 #ekstrakcja składowych dla pierwszej opinii z listy
                 for opinion in opinions:     
                     op = Opinion()
@@ -37,17 +41,17 @@ class Selectors(Enum):
 class Opinion:
     #lista składowych opinii wraz z selektorami i atrybutami
     selectors = {            
-            "author": ['div.reviewer-name-line'],
-            "recommendation": ['div.product-review-summary > em'],
-            "stars":['span.review-score-count'],
-            "content":['p.product-review-body'],
-            "pros":['div.pros-cell > ul'],
-            "cons":['div.cons-cell > ul'],
+            "author": ['span.user-post__author-name'],
+            "recommendation": ['span.user-post__author-recomendation > em'],
+            "stars":['span.user-post__score-count'],
+            "content":['div.user-post__text'],
+            "pros":['div.review-feature__col:has(div.review-feature__title--positives)'],
+            "cons":['div.review-feature__col:has(div.review-feature__title--negatives)'],
             "useful":['button.vote-yes', "data-total-vote"],
             "useless":['button.vote-no', "data-total-vote"],
-            "purchased":['div.product-review-pz'],
-            "purchase_date":['span.review-time > time:nth-of-type(1)',"datetime"],
-            "review_date":['span.review-time > time:nth-of-type(2)',"datetime"]    
+            "purchased":['div.review-pz'],
+            "purchase_date":['span.user-post__published > time:nth-of-type(1)',"datetime"],
+            "review_date":['span.user-post__published > time:nth-of-type(2)',"datetime"]    
                 }
     #konstruktor (inicjalizator) obiektu klasy            
     def __init__(self, opinion_id=None, author=None, recommendation=None, stars=None, content=None, 
@@ -66,12 +70,15 @@ class Opinion:
         self.reiew_date = review_date
     # reprezentacja tekstowa obiektu klasy
     def __str__(self):
-        return f'opinion id: {opinion_id}\n author: {author}\n' 
+        return f'product id: {self.product_id}\n nazwa: {self.name}\n' + '\n'.join(str(self.opinion) for opinion) 
     #reprezetnacja słownikowa obiektu   
     def __repr__(self):
         pass
+    def save_product(self):
+        pass
 
     def extract_opinion(self):
+        self.author = extract_feature(opinion, *self.selectors['author'])
         features = {key:extract_feature(opinion, *args)
                     for key,args in selectors.items()}
         self.opinion_id = int(opinion["data-entry-id"])
@@ -83,3 +90,8 @@ class Opinion:
                     features["content"] = features["content"].replace("\n", ". ").replace("\r",". ")
                     features["pros"] = remove_whitespaces(features["pros"])
                     features["cons"] = remove_whitespaces(features["cons"])
+   
+    product = Product("79688141")
+    product.extract_product()
+    print(product.opinions)
+    print(len(product.opinions))
